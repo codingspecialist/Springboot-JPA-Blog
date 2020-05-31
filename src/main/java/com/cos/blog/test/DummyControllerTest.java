5c62a7ff6442b4e6350120af7abadb61df2f7dcf
ssar <ssar@nate.com>
2020-05-31 21:02:53 +0900

    select 해보기

--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -1,6 +1,17 @@
 package com.cos.blog.test;
 
+import java.util.List;
+import java.util.function.Supplier;
+
+//import java.util.function.Supplier;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,12 +19,56 @@ import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
 import com.cos.blog.repository.UserRepository;
 
+// html파일이 아니라 data를 리턴해주는 controller = RestController
 @RestController
 public class DummyControllerTest {
 	
 	@Autowired // 의존성 주입(DI)
 	private UserRepository userRepository;
 	
+	// http://localhost:8000/blog/dummy/user
+	@GetMapping("/dummy/users")
+	public List<User> list(){
+		return userRepository.findAll();
+	}
+	
+	// 한페이지당 2건에 데이터를 리턴받아 볼 예정
+	@GetMapping("/dummy/user")
+	public List<User> pageList(@PageableDefault(size=2, sort="id", direction = Sort.Direction.DESC) Pageable pageable){
+		Page<User> pagingUser = userRepository.findAll(pageable);
+
+		List<User> users = pagingUser.getContent();
+		return users;
+	}
+	
+	// {id} 주소로 파마레터를 전달 받을 수 있음.
+	// http://localhost:8000/blog/dummy/user/5
+	@GetMapping("/dummy/user/{id}")
+	public User detail(@PathVariable int id) {
+		// user/4을 찾으면 내가 데이터베이스에서 못찾아오게 되면 user가 null이 될 것 아냐?
+		// 그럼 return null 이 리턴이 되자나.. 그럼 프로그램에 문제가 있지 않겠니?
+		// Optional로 너의 User객체를 감싸서 가져올테니 null인지 아닌지 판단해서 return해!!
+		
+//     // 람다식
+//		User user = userRepository.findById(id).orElseThrow(()->{
+//			return new IllegalArgumentException("해당 사용자는 없습니다.");
+//		});
+		
+		User user = userRepository.findById(id).orElseThrow(new Supplier<IllegalArgumentException>() {
+			@Override
+			public IllegalArgumentException get() {
+				return new IllegalArgumentException("해당 사용자가 없습니다.");
+			}
+		});
+		// 요청 : 웹브라우저
+		// user 객체 = 자바 오브젝트
+		// 변환 ( 웹브라우저가 이해할 수 있는 데이터) -> json (Gson 라이브러리)
+		// 스프링부트 = MessageConverter라는 애가 응답시에 자동 작동
+		// 만약에 자바 오브젝트를 리턴하게 되면 MessageConverter가 Jackson 라이브러리를 호출해서
+		// user 오브젝트를 json으로 변환해서 브라우저에게 던져줍니다.
+		return user;
+	}
+
 	// http://localhost:8000/blog/dummy/join (요청)
 	// http의 body에 username, password, email 데이터를 가지고 (요청)
 	@PostMapping("/dummy/join")
